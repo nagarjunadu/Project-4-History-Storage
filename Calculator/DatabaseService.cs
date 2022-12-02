9d0300222fde06a27517c9206d5d1ac70f887ecf
nagarjunadu <dnag198@gmail.com>
2022-12-02 11:30:23 -0600

    Changed Database services

--- Calculator/DatabaseService.cs
+++ Calculator/DatabaseService.cs
@@ -15,33 +15,5 @@ namespace Calculator
         public DatabaseService()
         {
         }
-
-        async Task Init()
-        {
-            if (Database is not null)
-                return;
-
-            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
-            var result = await Database.CreateTableAsync<HistoryModel>();
-        }
-
-        public async Task<List<HistoryModel>> GetItemsAsync()
-        {
-            await Init();
-            return await Database.Table<HistoryModel >().ToListAsync();
-        }
-
-        public async Task<int> SaveItemAsync(HistoryModel item)
-        {
-            await Init();
-            return await Database.InsertAsync(item);
-        }
-
-        public async Task<int> DeleteAllAsync()
-        {
-            await Init();
-            return await Database.DeleteAllAsync<HistoryModel>();
-        }
-
     }
 }
